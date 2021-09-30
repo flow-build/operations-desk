c4827fe5e98f6b90f931d5a12892194a13e3fe6c
Elson Oliveira <elsonfojr@gmail.com>
2021-09-30 10:37:11 -0300

    overflow and padding added to sidelist

--- src/pages/Dashboard/components/SideList/styles.js
+++ src/pages/Dashboard/components/SideList/styles.js
@@ -4,4 +4,8 @@ import { colors } from '../../../../styles';
 export const Container = styled.aside`
   grid-area: ${(props) => props.gridArea};
   background-color: ${colors.Gray_300};
+
+  padding: 1rem;
+
+  overflow-y: auto;
 `;
