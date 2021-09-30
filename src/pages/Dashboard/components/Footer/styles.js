96b046ad8c07aab41232f909532b592f3ddc462e
Elson Oliveira <elsonfojr@gmail.com>
2021-09-30 09:36:16 -0300

    Footer estilizado

--- src/pages/Dashboard/components/Footer/styles.js
+++ src/pages/Dashboard/components/Footer/styles.js
@@ -5,4 +5,12 @@ export const Container = styled.footer`
   grid-area: ${(props) => props.gridArea};
 
   background-color: ${colors.Primary};
+  color: ${colors.Gray_100};
+
+  display: flex;
+  align-items: center;
+
+  padding: 0 1rem;
+
+  font: 200 0.8rem Poppins;
 `;
