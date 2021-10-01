dd1850d62344c6263934cc921080e4f74273a336
Elson Oliveira <elsonfojr@gmail.com>
2021-10-01 17:44:38 -0300

    Sidelist showing reports dynamic

--- src/pages/Dashboard/components/SideList/index.js
+++ src/pages/Dashboard/components/SideList/index.js
@@ -1,5 +1,6 @@
 // @flow
 import React, { HTMLAttributes } from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 
 import * as S from './styles';
 import { Card } from '../../../../shared';
@@ -9,9 +10,30 @@ interface Props extends HTMLAttributes<HTMLElement> {
 }
 
 export const SideList: React.FC<Props> = ({ gridArea, ...props }) => {
+  const { currentScreen } = useSelector(
+    (state) => state.pageNavigation,
+    shallowEqual
+  );
+
+  const items = getItems(currentScreen);
+
+  function getItems(screen: string): Array<any> {
+    if (screen === 'Relatórios') {
+      const reports = JSON.parse(localStorage.getItem('@reports'));
+      return reports.map((r) => ({
+        title: r.title,
+        description: r.description,
+      }));
+    }
+
+    return [];
+  }
+
   return (
     <S.Container gridArea={gridArea}>
-      <Card />
+      {items.map((i) => (
+        <Card title={i.title} description={i.description} />
+      ))}
     </S.Container>
   );
 };
