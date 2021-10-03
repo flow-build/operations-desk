e30b8be3f4b4fffdbb8c3d929950d655f8efaa01
Elson Oliveira <elsonfojr@gmail.com>
2021-10-03 00:36:40 -0300

    Changing generic component on loading page

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import loadingGif from '../../assets/images/loading.gif';
+import { Loading as LoadingGIF } from '../../shared';
 
 import * as S from './styles';
 
 export const Loading = () => {
   return (
     <S.Container>
-      <img src={loadingGif} alt="Loading" />
+      <LoadingGIF />
     </S.Container>
   );
 };
--- src/pages/Loading/styles.js
+++ src/pages/Loading/styles.js
@@ -1,18 +1,3 @@
 import styled from 'styled-components';
-import { colors } from '../../styles';
 
-export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  background-color: ${colors.Primary_100};
-
-  & > * {
-    width: 70px;
-    height: 70px;
-  }
-`;
+export const Container = styled.div``;
