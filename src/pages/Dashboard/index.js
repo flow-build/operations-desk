f17bc98c3849674cf26fe09192f2c06819890c7c
Elson Oliveira <elsonfojr@gmail.com>
2021-10-04 17:34:25 -0300

    Dashboard receiving data

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+/* Components */
 import { Content, Footer, Header, SideList } from './components';
+import { Loading as LoadingGIF } from '../../shared';
+
+/* Data */
+import { DASHBOARD_FAKE } from '../../data';
 
 import * as S from './styles';
 
 export const Dashboard = () => {
+  // Params da página
+  const { state } = useLocation();
+  console.log({ state });
+
+  useEffect(() => {
+    localStorage.setItem('@reports', JSON.stringify(state?.reports));
+  }, [state]);
+
+  const { headerItems } = DASHBOARD_FAKE;
+
   return (
     <S.Container>
-      <Header gridArea="HE"></Header>
-      <SideList gridArea="SL"></SideList>
-      <Content gridArea="CT"></Content>
+      <Header gridArea="HE" items={headerItems} />
+      <SideList gridArea="SL" />
+      <Content gridArea="CT" />
       <Footer gridArea="FT">
         Versão 0.0.1 - Processo em foco: 9a038a0d0a90-d8adasd9ads9 - Número de
         atividades: 4
       </Footer>
+
+      {/* <LoadingGIF /> */}
     </S.Container>
   );
 };
