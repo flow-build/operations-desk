9f92efc11f93841446f33fba118680b02764721c
Elson Oliveira <elsonfojr@gmail.com>
2021-10-04 17:33:04 -0300

    Content showing json schema

--- src/pages/Dashboard/components/Content/index.js
+++ src/pages/Dashboard/components/Content/index.js
@@ -1,16 +1,59 @@
 // @flow
 import React, { HTMLAttributes } from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
+import Form from '@rjsf/material-ui';
 
 import * as S from './styles';
 
 interface Props extends HTMLAttributes<HTMLElement> {
   gridArea: string;
+  content: any;
+  isReport?: boolean;
 }
 
-export const Content: React.FC<Props> = ({ gridArea, ...props }) => {
+export const Content: React.FC<Props> = ({
+  gridArea,
+  content,
+  isReport,
+  ...props
+}) => {
+  const { selectedActivity } = useSelector(
+    (state) => state.pageNavigation,
+    shallowEqual
+  );
+
+  const isJsonSchema = !!selectedActivity?.props?.result?.jsonSchema;
+  const formSchema = getFormSchema();
+
+  console.log('selectedActivity', selectedActivity);
+
+  function getFormSchema() {
+    if (isJsonSchema) {
+      const schema = selectedActivity?.props?.result;
+      return { jsonSchema: schema.jsonSchema, uiSchema: schema.uiSchema };
+    }
+
+    return { jsonSchema: null, uiSchema: null };
+  }
+
+  // isReport && console.log('content', JSON.parse(content)[0]);
+  /* {isReport && (
+          <S.ReportContent
+            dangerouslySetInnerHTML={{ __html: JSON.parse(content)[0].embed }}
+          />
+        )} */
+
   return (
     <S.Container gridArea={gridArea}>
-      <S.Pane></S.Pane>
+      <S.Pane>
+        {isJsonSchema && (
+          <Form
+            schema={formSchema.jsonSchema}
+            uiSchema={formSchema.uiSchema}
+            onSubmit={(e) => console.log('e', e)}
+          />
+        )}
+      </S.Pane>
     </S.Container>
   );
 };
--- src/pages/Dashboard/components/Content/styles.js
+++ src/pages/Dashboard/components/Content/styles.js
@@ -22,3 +22,7 @@ export const Pane = styled.div`
 
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `;
+
+export const ReportContent = styled.div`
+  height: 100%;
+`;
