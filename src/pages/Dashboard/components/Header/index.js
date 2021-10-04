cd1e3daced73dc55286e92cc00d6d2a2d9c73fc1
Elson Oliveira <elsonfojr@gmail.com>
2021-10-04 17:32:43 -0300

    Dynamic header

--- src/pages/Dashboard/components/Header/index.js
+++ src/pages/Dashboard/components/Header/index.js
@@ -2,20 +2,31 @@
 import React, { HTMLAttributes } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { setCurrentScreen } from '../../../../redux/pageNavigation.slice';
+import {
+  setCurrentFilter,
+  setCurrentScreen,
+} from '../../../../redux/pageNavigation.slice';
 
 import * as S from './styles';
 
+interface HeaderItem {
+  aba: string;
+  label: string;
+  wfFilter: any[];
+}
+
 interface Props extends HTMLAttributes<HTMLElement> {
+  items: HeaderItem[];
   gridArea: string;
 }
 
-export const Header: React.FC<Props> = React.memo(({ gridArea }) => {
+export const Header: React.FC<Props> = React.memo(({ items, gridArea }) => {
   const dispatch = useDispatch();
 
-  function handleClick(page: string) {
+  async function handleClick(page: string) {
     try {
-      dispatch(setCurrentScreen(page));
+      await dispatch(setCurrentFilter({ items, aba: page }));
+      await dispatch(setCurrentScreen(page));
     } catch (error) {
       const message = 'Erro ao tentar mudar a tela exibida.';
       console.error(message, error);
@@ -25,11 +36,14 @@ export const Header: React.FC<Props> = React.memo(({ gridArea }) => {
   return (
     <S.Container gridArea={gridArea}>
       <S.Menu>
-        <S.MenuItems onClick={() => handleClick('Home')}>Home</S.MenuItems>
-        <S.MenuItems onClick={() => handleClick('Sobre')}>Sobre</S.MenuItems>
-        <S.MenuItems onClick={() => handleClick('Relatórios')}>
-          Relatórios
-        </S.MenuItems>
+        {items.map((item, index) => (
+          <S.MenuItems
+            key={index.toString()}
+            onClick={() => handleClick(item.aba)}
+          >
+            {item.label}
+          </S.MenuItems>
+        ))}
       </S.Menu>
       <S.Logout />
     </S.Container>
