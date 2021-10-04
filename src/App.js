80a457ccbaa6357b5e21503a8704d98a9ba74022
Elson Oliveira <elsonfojr@gmail.com>
2021-10-04 17:35:49 -0300

    Workflow Manager added to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Routes } from './routers';
 import GlobalStyles from './styles/GlobalStyle';
 
+import { Provider } from 'react-redux';
+import { WorkflowManager } from '@flowbuild/redux-toolkit-workflow-manager';
+
+import { store } from './redux';
+
 function App() {
   return (
-    <>
-      <Routes />
-      <GlobalStyles />
-    </>
+    <Provider store={store}>
+      <WorkflowManager>
+        <Routes />
+        <GlobalStyles />
+      </WorkflowManager>
+    </Provider>
   );
 }
 
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Dashboard, SignIn } from '../pages';
+// @flow
+import React, { useEffect } from 'react';
+import { useWorkflowManager } from '@flowbuild/redux-toolkit-workflow-manager/useWorkflowManager';
+import { BrowserRouter, Route, Switch, useHistory } from 'react-router-dom';
+import { Dashboard, Loading, SignIn } from '../pages';
+
+import { ACTION_TO_ROUTER } from '../config';
+
+interface Activity {
+  activities: Array<any>;
+  bag: Object;
+  created_at: string;
+  id: string;
+  process_id: string;
+  process_state_id: string;
+  props: { action: string, result: Object };
+  status: string;
+  type: string;
+  workflow_id: string;
+  workflow_name: string;
+}
+
+const NestedRoutes = () => {
+  const history = useHistory();
+  const { currentActivity, login } = useWorkflowManager();
+
+  useEffect(() => {
+    handleLogin();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (currentActivity) {
+      console.log('currentActivity', currentActivity);
+      handleNavigation(currentActivity);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentActivity]);
+
+  async function handleLogin() {
+    try {
+      const body = { claims: ['anonymous'] };
+      await login('http://3.82.154.55:3000/token', body);
+    } catch (error) {
+      const message = 'Erro ao tentar fazer login anônimo.';
+      console.error(message, error);
+    }
+  }
+
+  async function handleNavigation(activity: Activity): void {
+    const {
+      props: { action, result },
+    } = activity;
+
+    const router = ACTION_TO_ROUTER[action];
+
+    if (router) {
+      history.push(router, result);
+    }
+  }
+
+  return (
+    <Switch>
+      <Route path="/" exact component={Loading} />
+      <Route path="/login" component={SignIn} />
+      <Route path="/dashboard" component={Dashboard} />
+    </Switch>
+  );
+};
 
 export const Routes = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" component={Dashboard} />
+      <NestedRoutes />
     </BrowserRouter>
   );
 };
